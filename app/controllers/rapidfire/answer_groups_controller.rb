9bced82e101c0d585d088c4145fe7243e2aa036b
Abhirama sundram <abhiramasundram2007@gmail.com>
2014-08-11 19:30:00 +0530

    change in answer_groups

--- app/controllers/rapidfire/answer_groups_controller.rb
+++ app/controllers/rapidfire/answer_groups_controller.rb
@@ -4,6 +4,10 @@ module Rapidfire
 
     def new
       @answer_group_builder = AnswerGroupBuilder.new(answer_group_params)
+      @device = Device.find_by_device_id(params[:device_id])
+      if @device and @device.authorized
+        @child = @device.parent.children.first
+      end
     end
 
     def create
@@ -23,7 +27,15 @@ module Rapidfire
 
     def answer_group_params
       answer_params = { params: params[:answer_group] }
+      # Have changed current_user to parent since we dont have user session for parent
+      if params[:child_id]
+        child = Child.find_by_id(params[:child_id])
+
+      answer_params.merge(user: child, question_group: @question_group)
+    else
       answer_params.merge(user: current_user, question_group: @question_group)
+    end
+      
     end
   end
 end
