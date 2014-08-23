7358313a90ea3938b6d2ccbaca9bf7789817db17
Abhirama sundram <abhiramasundram2007@gmail.com>
2014-08-23 20:01:34 +0530

    remove form types from repo"

--- app/controllers/rapidfire/questions_controller.rb
+++ app/controllers/rapidfire/questions_controller.rb
@@ -15,13 +15,7 @@ module Rapidfire
       @question = QuestionForm.new(:question_group => @question_group)
       @question_group = QuestionGroup.find(params[:question_group_id])
       
-      if @question_group.form_type == "multi"
-        @form_types = ::Rapidfire::QuestionForm::QUESTION_TYPES.except("Radio", "Short")
-      elsif @question_group.form_type == "yn"
-        @form_types = ::Rapidfire::QuestionForm::QUESTION_TYPES.except("Checkbox", "Short")
-      else
-        @form_types = ::Rapidfire::QuestionForm::QUESTION_TYPES.except("Radio", "Checkbox")
-      end
+     
       
       # To bring form show page in add field page
       answer_params = {}
